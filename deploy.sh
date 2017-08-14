95d1d25732ce754ff9e3e02f1a333dd45c2cc370
Darren Cuzner <darren.cuzner@telrock.com>
2017-08-14 21:01:25 +0100

    testing depolyment: created deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-echo "stopping running application"
+# echo "stopping running application"
 
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop dodsv'
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm dodsv'
+# ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop dodsv'
+# ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm dodsv'
 
 echo "pulling latest version of the code"
-
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull cuznerdexter/cuz-dex-hub:latest'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull cuznerdexter/cuzdex-hub:latest'
 
 echo "starting new version"
-
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name dodsv -p 3000:4000 cuznerdexter/cuz-dex-hub:latest'
-
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name dodsv -p 3000:4000 cuznerdexter/cuzdex-hub:latest'
 echo "success!"
 
 exit 0
