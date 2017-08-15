0714cdfb898470b1a7a393998d8e16054a8c823a
Darren Cuzner <darren.cuzner@telrock.com>
2017-08-15 22:42:12 +0100

    testing deployment: deploy.sh add docker-compose up

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-# echo "stopping running application"
+echo "stopping running application"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose down cuznerdexter/cuzdex-hub:latest'
 
 # ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop dodsv'
 # ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm dodsv'
 
-echo "pulling latest version of the code"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull cuznerdexter/cuzdex-hub:latest'
+# echo "pulling latest version of the code"
+# ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull cuznerdexter/cuzdex-hub:latest'
 
-echo "starting new version"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name dodsv -p 3000:5000 cuznerdexter/cuzdex-hub:latest'
-echo "success!"
+# echo "starting new version"
+# ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always --name dodsv -p 3000:5000 cuznerdexter/cuzdex-hub:latest'
+# echo "success!"
+
+echo "pulling latest version of the code"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose up cuznerdexter/cuzdex-hub:latest'
 
 exit 0
